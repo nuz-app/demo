2d2b65a8ce310e42767c258cd3516e7ee73ae7e9
Hieu Lam <lamhieu.vk@gmail.com>
2020-05-16 21:35:16 +0700

    feat: fix issue on safari handling promise

--- pages/index.js
+++ pages/index.js
@@ -20,10 +20,10 @@ const IndexPage = () => {
         <title>Demo website using Nuz</title>
         <link rel="shortcut icon" href="/static/favicon.ico" />
         <meta name="description" content="Demo basic features of Nuz project using Next.js" />
-        <meta name="image" content="/static/image.png" />
+        <meta name="image" content="https://demo.nuz.app/static/image.png" />
         <meta property="og:title" content="Demo website using Nuz" />
         <meta property="og:description" content="Demo basic features of Nuz project using Next.js" />
-        <meta property="og:image" content="/static/image.png" />
+        <meta property="og:image" content="https://demo.nuz.app/static/image.png" />
         <meta property="og:url" content="https://demo.nuz.app" />
         <link rel="stylesheet" type="text/css" href="https://unpkg.com/normalize.css@8.0.1/normalize.css" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap" rel="stylesheet" />
