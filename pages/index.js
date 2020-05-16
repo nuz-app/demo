10a0f57837455799277a0659cf62333996b91c68
Hieu Lam <lamhieu.vk@gmail.com>
2020-05-16 20:38:53 +0700

    chore: update image, favicon and description for website

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,13 @@ const IndexPage = () => {
     <>
       <Head>
         <title>Demo website using Nuz</title>
-        <meta name="description" content="Demo features of Nuz project in production mode" />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <meta name="description" content="Demo basic features of Nuz project using Next.js" />
+        <meta name="image" content="/static/image.png" />
+        <meta property="og:title" content="Demo website using Nuz" />
+        <meta property="og:description" content="Demo basic features of Nuz project using Next.js" />
+        <meta property="og:image" content="/static/image.png" />
+        <meta property="og:url" content="https://demo.nuz.app" />
         <link rel="stylesheet" type="text/css" href="https://unpkg.com/normalize.css@8.0.1/normalize.css" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap" rel="stylesheet" />
       </Head>
