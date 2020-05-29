786096322e9852601c18e8f97585aca856136f26
Hieu Lam <lamhieu.vk@gmail.com>
2020-05-29 19:46:24 +0700

    feat: use new nuz version and reduce config in `_app.js`

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,20 @@
 import * as nuz from '@nuz/core'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import * as ReactRedux from 'react-redux'
 import Link from 'next/link'
-import dynamic from 'next/dynamic'
 
 import { withRedux } from '../lib/redux'
-
-nuz.reactIntegrate()
 
 nuz.bootstrap({
   compose: 'nuz-demo',
   ssr: true,
-  // Link only run on development mode
+  // Only use workspace dev mode in `development` mode
   linked: {
     port: 4000,
   },
   vendors: {
-    '@nuz/core': nuz,
-    'react': React,
-    'react-dom': ReactDOM,
     'react-redux': ReactRedux,
     'next/link': Link,
-    'next/dynamic': dynamic,
   },
   preload: [
     '@nuz-demo/navigation',
